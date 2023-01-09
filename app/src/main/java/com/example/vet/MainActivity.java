2ec483691e6dbcda9e2a9a2079a46b85c0db7573
kenji <a19310159@live.ceti.mx>
2023-01-08 20:06:48 -0600

    login

--- app/src/main/java/com/example/vet/MainActivity.java
+++ app/src/main/java/com/example/vet/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.text.TextUtils;
@@ -19,54 +20,23 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText TxtUsuario;
-    private EditText TxtPass;
-    private Button btncrespo;
-    FirebaseAuth mAuth;
+    Button btnregistrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAuth = FirebaseAuth.getInstance();
-        TxtUsuario = (EditText) findViewById(R.id.TxtUsuario);
-        TxtPass = (EditText) findViewById(R.id.TxtPass);
-        btncrespo = (Button) findViewById(R.id.btncrespo);
+        btnregistrar = findViewById(R.id.btnRegistrar);
 
-        btncrespo.setOnClickListener(new View.OnClickListener() {
+        btnregistrar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String email,password;
-                email = String.valueOf(TxtUsuario.getText());
-                password = String.valueOf(TxtPass.getText());
-                if(TextUtils.isEmpty(email)){
-                    Toast.makeText(MainActivity.this,"no com",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(TextUtils.isEmpty(password)){
-                    Toast.makeText(MainActivity.this,"no pass",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                mAuth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                  Toast.makeText(MainActivity.this,"registrado",
-                                          Toast.LENGTH_SHORT).show();
-
-                                } else {
-                                    // If sign in fails, display a message to the user.
-
-                                    Toast.makeText(MainActivity.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-
-                                }
-                            }
-                        });
-
+            public void onClick(View v) {
+                Intent regist = new Intent(MainActivity.this,RegisterAct.class);
+                startActivity(regist);
             }
         });
+    }
+
     }
 
 
@@ -76,8 +46,3 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
-
-
-}
