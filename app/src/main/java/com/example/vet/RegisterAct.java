2182954d43fea2283bab63db5f540c9718333291
KenjiOro <102265817+KenjiOro@users.noreply.github.com>
2023-02-05 17:07:36 -0600

    Registro basico

--- app/src/main/java/com/example/vet/RegisterAct.java
+++ app/src/main/java/com/example/vet/RegisterAct.java
@@ -1,16 +1,21 @@
 package com.example.vet;
 
+import static com.basgeekball.awesomevalidation.ValidationStyle.BASIC;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.basgeekball.awesomevalidation.AwesomeValidation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -18,9 +23,10 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterAct extends AppCompatActivity {
     private EditText TxtUsuario;
-    private EditText TxtPass;
+    private EditText TxtPass, TxtPass2;
     private Button btncrespo;
     FirebaseAuth mAuth;
+    AwesomeValidation awesomeValidation;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -31,43 +37,60 @@ public class RegisterAct extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         TxtUsuario = (EditText) findViewById(R.id.txtUsuario);
         TxtPass = (EditText) findViewById(R.id.txtPass);
+        TxtPass2 = (EditText) findViewById(R.id.txtPass2);
         btncrespo = (Button) findViewById(R.id.btncrespo);
+        awesomeValidation = new AwesomeValidation(BASIC);
+        awesomeValidation.addValidation(this, R.id.txtCorreo, Patterns.EMAIL_ADDRESS, R.string.err_email);
+        awesomeValidation.addValidation(this, R.id.txtPass, ".{6,}", R.string.err_pass);
+        awesomeValidation.addValidation(this, R.id.txtPass2, ".{6,}", R.string.err_pass);
+
 
         btncrespo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email, password;
-                email = String.valueOf(TxtUsuario.getText());
-                password = String.valueOf(TxtPass.getText());
-                if (TextUtils.isEmpty(email)) {
-                    Toast.makeText(RegisterAct.this, "no com", Toast.LENGTH_SHORT).show();
-                    return;
+
+                if (awesomeValidation.validate()) {
+                    String email, password, password2;
+                    email = String.valueOf(TxtUsuario.getText());
+                    password = String.valueOf(TxtPass.getText());
+                    password2 = String.valueOf(TxtPass2.getText());
+
+                    if (password.equals(password2)) {
+
+                        mAuth.createUserWithEmailAndPassword(email, password)
+                                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(RegisterAct.this, "Registrado con exito",Toast.LENGTH_SHORT).show();
+                                            volverLogin();
+                                        } else {
+                                            // If sign in fails, display a message to the user.
+
+                                            Toast.makeText(RegisterAct.this, "Ya existe una cuenta con ese correo",
+                                                    Toast.LENGTH_SHORT).show();
+
+                                        }
+                                    }
+                                });
+
+                    } else {        TxtPass2.setError("No coiniden");
+
+                    }
+
                 }
-                if (TextUtils.isEmpty(password)) {
-                    Toast.makeText(RegisterAct.this, "no pass", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                mAuth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(RegisterAct.this, "registrado",
-                                            Toast.LENGTH_SHORT).show();
-
-                                } else {
-                                    // If sign in fails, display a message to the user.
-
-                                    Toast.makeText(RegisterAct.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-
-                                }
-                            }
-                        });
 
             }
+
         });
 
 
     }
+
+    private void volverLogin (){
+        Intent iniciar = new Intent(this,MainActivity.class);
+        startActivity(iniciar);
+        finish();
+    }
+
 }
