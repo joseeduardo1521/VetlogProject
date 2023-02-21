3a36d5dd43b41e12a517c5d7739ef8d37f441e3b
kenji <a19310159@live.ceti.mx>
2023-02-20 20:06:32 -0600

    Inicio de registro usuarios administradores

--- app/src/main/java/com/example/vet/Menu.java
+++ app/src/main/java/com/example/vet/Menu.java
@@ -3,12 +3,25 @@ package com.example.vet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Menu extends AppCompatActivity {
+
+    private View cardPersonal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        cardPersonal = (View) findViewById(R.id.D4);
+
+
+        cardPersonal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
