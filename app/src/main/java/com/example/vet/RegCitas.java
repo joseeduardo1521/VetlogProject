f62bdfa615a750aeef2ac17b72a9fd62e5933d1b
Eduardo Oropeza <josee375@outlook.com>
2023-03-05 16:34:47 -0600

    TimePicker para seleccionar hora de citas

--- app/src/main/java/com/example/vet/RegCitas.java
+++ app/src/main/java/com/example/vet/RegCitas.java
@@ -4,24 +4,59 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.TimeZone;
 
 public class RegCitas extends AppCompatActivity {
 
     private DatePickerDialog datePickerDialog;
+    private TimePickerDialog timePickerDialog;
     private Button dateButton;
+    private Button timeButton;
+    private EditText textoss, hor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg_citas);
         initDatePicker();
         dateButton = findViewById(R.id.dataPickerButton);
+        textoss = (EditText) findViewById(R.id.fechitas);
+        hor = (EditText) findViewById(R.id.horitas);
         dateButton.setText(getTodaysDate());
+        timeButton = findViewById(R.id.TimePickerButton);
+
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int hours = calendar.get(Calendar.HOUR_OF_DAY);
+                int mins = calendar.get(Calendar.MINUTE);
+                TimePickerDialog timePickerDialog = new TimePickerDialog(RegCitas.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Calendar c = Calendar.getInstance();
+                        c.set(Calendar.HOUR_OF_DAY,hourOfDay);
+                        c.set(Calendar.MINUTE,minute);
+                        c.setTimeZone(TimeZone.getDefault());
+                        SimpleDateFormat format = new SimpleDateFormat("k:mm a");
+                        String time = format.format(c.getTime());
+                        hor.setText(time);
+
+                    }
+                },hours, mins, false);
+                timePickerDialog.show();
+            }
+        });
+
     }
 
     private String getTodaysDate() {
@@ -33,6 +68,7 @@ public class RegCitas extends AppCompatActivity {
         return makeDateString(day,month,year);
     }
 
+
     private void initDatePicker(){
         DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener(){
             @Override
@@ -40,6 +76,7 @@ public class RegCitas extends AppCompatActivity {
                 month = month +1;
                 String date = makeDateString(day,month,year);
                 dateButton.setText(date);
+                textoss.setText(date);
             }
         };
         Calendar cal = Calendar.getInstance();
@@ -89,5 +126,6 @@ public class RegCitas extends AppCompatActivity {
     public void openDatePicker(View view){
         datePickerDialog.show();
     }
+
 
 }
