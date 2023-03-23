0d0f773e33a8d947ecd83342f5399c42955eff9d
Eduardo Oropeza <josee375@outlook.com>
2023-03-22 18:42:09 -0600

    diseño usuario, checar conexion wifi(no implementado)

--- app/src/main/java/com/example/vet/SplashScreen.java
+++ app/src/main/java/com/example/vet/SplashScreen.java
@@ -2,16 +2,23 @@ package com.example.vet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
+    BroadcastReceiver broadcastReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        broadcastReceiver = new ConnectionReceiver();
+        registoreNetworkBroadcast();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -23,5 +30,25 @@ public class SplashScreen extends AppCompatActivity {
 
 
 
+    }
+
+    protected void registoreNetworkBroadcast(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+            registerReceiver(broadcastReceiver,new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+        }
+    }
+
+    protected void unregistorNetwork(){
+        try {
+            unregisterReceiver(broadcastReceiver);
+        }catch (IllegalArgumentException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregistorNetwork();
     }
 }
---
+++ app/src/main/java/com/example/vet/Usuario_Menu.java
@@ -0,0 +1,14 @@
+package com.example.vet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Usuario_Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario_menu);
+    }
+}
