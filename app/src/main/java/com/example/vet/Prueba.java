617e5238b1c417d36a4bbd4e46b61e741d44e535
Eduardo Oropeza <josee375@outlook.com>
2023-04-04 19:11:09 -0600

    Button recargar en checar conexion

--- app/src/main/java/com/example/vet/Prueba.java
+++ app/src/main/java/com/example/vet/Prueba.java
@@ -3,23 +3,38 @@ package com.example.vet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Prueba extends AppCompatActivity {
     TextView nofi;
+    Button reclage;
     Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prueba);
         nofi = findViewById(R.id.nofifi);
+        reclage = findViewById(R.id.btnReload);
 
         if(!isConnected()){
             nofi.setVisibility(View.VISIBLE);
+
         }
+
+        reclage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Prueba.this,SplashScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
 
     }
     private boolean isConnected(){
